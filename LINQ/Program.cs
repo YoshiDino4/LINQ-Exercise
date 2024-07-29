5b7d768583cfec03fb0e49a06c560bf5144dc4db
Yash Bajaj <yash@bajajmd.net>
2024-07-29 12:54:57 -0400

    Competed the video game list exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "The Legend of Zelda",
+                "Assetto Corsa",
+                "GTA 5",
+                "Sea of Thieves",
+                "Halo: Reach",
+                "Cyberpunk 2077",
+                "Baldur's Gate 3",
+                "Elden Ring"
+            };
+
+            List<string> gamesOrder = videoGames.OrderBy(game => game.Length).ToList();
+            
+            Console.WriteLine("Ordered list by title length");
+            foreach (var game in gamesOrder)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
